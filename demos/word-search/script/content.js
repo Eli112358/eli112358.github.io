a4ca4d69190a637f9d83d3e64d6ecd9c5a66b2e1
Eli112358 <eli112358.sarp@hotmail.com>
2016-02-16 07:29:29 -0500

    Added grid filler

--- demos/word-search/script/content.js
+++ demos/word-search/script/content.js
@@ -1,6 +1,7 @@
 ﻿var words;
 var numCols;
 var numRows;
+var grid;
 function getById(id) {
 	return document.getElementById(id);
 }
@@ -13,5 +14,22 @@ function init() {
 		getById("difficulty").innerHtml=obj.difficulty;
 		numRows=obj.rows;
 		numCols=obj.columns;
+		grid=getById("grid");
+		var alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";//change this later to support different languages
+		for(var x=0;x<numRows;x++) {
+			var row=document.createElement("tr");
+			for(var y=0;y<numCols;y++) {
+				var cell=document.createElement("td");
+				cell.id="cell-"+x+"-"+y;
+				var anchor=document.createElement("a");
+				anchor.id="anchor-"+x+"-"+y;
+				anchor.href="#";
+				anchor.onclick="click("+x+","+y+")";
+				anchor.innerHtml=parseInt(Math.random()*alphabet.length);
+				cell.appendChild(anchor);
+				row.appendChild(cell);
+			}
+			grid.appendChild(row);
+		}
 	});
 }
