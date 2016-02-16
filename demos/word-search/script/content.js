f4fbc5667bccf53f3ea05f976a73a72a42b38895
Eli112358 <eli112358.sarp@hotmail.com>
2016-02-16 11:43:37 -0500

    Added click and createChild functions in content.js Removed the anchor var Fixed some issues and typos

--- demos/word-search/script/content.js
+++ demos/word-search/script/content.js
@@ -5,31 +5,36 @@ var grid;
 function getById(id) {
 	return document.getElementById(id);
 }
+function createChild(parent,type) {
+	var child=document.createElement(type);
+	parent.appendChild(child);
+	return child;
+}
+function click(cell) {
+	var classes=cell.className;
+	var x=parseInt(classes.substr(classes.indexOf("row")+3,1));
+	var y=parseInt(classes.substr(classes.indexOf("col")+3,1));
+	console.log("Clicked on "+x+","+y);
+}
 function init() {
 	var id=getURLParameter("id");
 	getFile("themes/"+id+".json",function(data) {
 		var obj=JSON.parse(data);
 		words=obj.words;
-		getById("theme").innerHtml=obj.theme;
-		getById("difficulty").innerHtml=obj.difficulty;
+		getById("theme").innerHTML=obj.theme;
+		getById("difficulty").innerHTML=obj.difficulty;
 		numRows=obj.rows;
-		numCols=obj.columns;
+		numCols=obj.colunms;
 		grid=getById("grid");
 		var alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";//change this later to support different languages
 		for(var x=0;x<numRows;x++) {
-			var row=document.createElement("tr");
+			var row=createChild(grid,"tr");
 			for(var y=0;y<numCols;y++) {
-				var cell=document.createElement("td");
-				cell.id="cell-"+x+"-"+y;
-				var anchor=document.createElement("a");
-				anchor.id="anchor-"+x+"-"+y;
-				anchor.href="#";
-				anchor.onclick="click("+x+","+y+")";
-				anchor.innerHtml=parseInt(Math.random()*alphabet.length);
-				cell.appendChild(anchor);
-				row.appendChild(cell);
+				var cell=createChild(row,"td");
+				cell.innerHTML=alphabet.charAt(parseInt(Math.random()*alphabet.length));
+				cell.className="row"+x+" col"+y;
+				cell.onclick=function() {click(this)};
 			}
-			grid.appendChild(row);
 		}
 	});
 }
