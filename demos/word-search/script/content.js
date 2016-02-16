5a44c7f491cc446e657fc9e90d81cd9ebb09b188
Eli112358 <eli112358.sarp@hotmail.com>
2016-02-16 07:10:50 -0500

    Added info getting code

--- demos/word-search/script/content.js
+++ demos/word-search/script/content.js
@@ -1,10 +1,17 @@
-﻿var words=new Array();
+﻿var words;
+var numCols;
+var numRows;
 function getById(id) {
 	return document.getElementById(id);
 }
 function init() {
 	var id=getURLParameter("id");
 	getFile("/"+id+".json",function(data) {
-		
+		var obj=JSON.parse(data);
+		words=obj.words;
+		getById("theme").innerHtml=obj.theme;
+		getById("difficulty").innerHtml=obj.difficulty;
+		numRows=obj.rows;
+		numCols=obj.columns;
 	});
 }
