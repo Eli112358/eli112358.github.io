897ae736ebf1c3f6f1af0080026f23977066565b
Eli112358 <Eli112358.SARP@hotmail.com>
2016-10-11 04:46:33 -0400

    Began using cookie data and bug fix

--- javascripts/repo.js
+++ javascripts/repo.js
@@ -1,5 +1,5 @@
 function getPath(repo,file) {
-	return "repositories/${repo}/"+file;
+	return `repositories/${repo}/${file}`;
 }
 function getById(id) {
 	return document.getElementById(id);
@@ -10,19 +10,31 @@ function loadRepo() {
 	var isFile=typeof file=="undefined";
 	var repoName=getById("repo-name");
 	var release=getById("release");
-	repoName.href="https://github.com/eli112358/${repo}";
+	repoName.href="https://github.com/eli112358/"+repo;
 	repoName.innerHTML=repo.replace('-',' ');
-	release.href="${repoName.href}/releases/latest";
+	release.href=repoName.href+"/releases/latest";
 	document.title=title+" - "+(isFile?file:repo);
 	if(isFile) {
 		var usage=getById("usage");
 		var fileName=getById("file-name");
-		var pref=getCookie("descriptionStyle");
-		fileName.href="${repoName.href}/blob/master/${file}";
+		var prefOptions=["indented","inline","tooltip"];
+		var cname="descriptionLocation";
+		if(getCookie(cname)=="")setCookie(cname,prefOptions[0],365);
+		var pref=getCookie(cname);
+		fileName.href=repoName.href+"/blob/master/"+file;
 		fileName.innerHTML=file;
-		getFile(file+".json",function(data){
+		getFile(getPath(file+".json"),function(data){
 			function loadDescription(item){}
-			function loadItem(item){}
+			function loadItem(item){
+				switch(prefOptions.indexOf(pref)) {
+					case 0:
+					case 1:
+					case 2:
+					default:
+						consoel.log(`Invalid value for ${cname}: `+pref);
+				}
+				var htmlCode="";
+			}
 			var obj=JSON.parse(data.replace('\n',''));
 			for(var item in obj.items)loadItem(item);
 		});
@@ -31,7 +43,7 @@ function loadRepo() {
 		getFile(getPath(repo,"files.txt"),function(data) {
 			var lines=data.split('/n');
 			for(var x=0;x<lines.length;x++){
-				filesList.innerHTML+="<li><a href=\"usage.html?repo=${repo}&file=${lines[x]}\">${lines[x]}</a></li>";
+				filesList.innerHTML+=`<li><a href="usage.html?repo=${repo}&file=${lines[x]}">${lines[x]}</a></li>`;
 			}
 		});
 	}
