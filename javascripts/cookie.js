94e922828987b6105a37844f469f342964ad1bb7
Eli112358 <Eli112358.SARP@hotmail.com>
2016-10-11 04:43:28 -0400

    Code golf and bug fix

--- javascripts/cookie.js
+++ javascripts/cookie.js
@@ -1,22 +1,15 @@
 function setCookie(cname,cvalue,exdays) {
 	var d=new Date();
 	d.setTime(d.getTime()+(exdays*24*60*60*1000));
-	var expires="expires="+d.toUTCString();
-	document.cookie=cname+"="+cvalue+"; "+expires;
+	document.cookie=`${cname}=${cvalue}; expires=${d.toUTCString()}`;
 }
 function getCookie(cname) {
 	var name=cname+"=";
-	var ca=document.cookie.split(';');
-	for(var i=0;i<ca.length;i++) {
-		var c=ca[i];
-		while (c.charAt(0)==' ') c=c.substring(1);
-		if(c.indexOf(name)==0) return c.substring(name.length,c.length);
-	}
-	return "";
+	if(document.cookie.indexOf(name)==-1)return "";
+	var sliced=document.cookie.substring(document.cookie.indexOf(name)+name.length);
+	return sliced.substr(0,sliced.indexOf(";"));
 }
 function checkCookie(cname) {
 	var cvalue=getCookie(cname);
-	if(cvalue!=""&&cvalue!=null) {
-		setCookie(cname,cvalue,365);
-	}
+	if(cvalue!=""&&cvalue!=null)setCookie(cname,cvalue,365);
 }
