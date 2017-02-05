fa0470e71f0c569a8befb4199baff35132b6583e
Eli112358 <Eli112358.SARP@hotmail.com>
2017-02-05 06:05:33 -0500

    Minor tweaks

--- javascripts/fetchRemoteFile.js
+++ javascripts/fetchRemoteFile.js
@@ -1,17 +1,18 @@
 function getFile(path,handler) {
+	var getDate=function() {return new Date().toString().substring(16,24)};
 	var xhttp=new XMLHttpRequest();
 	xhttp.onreadystatechange=function () {
 		if(xhttp.readyState==4&&xhttp.status==200) {
-			console.log("["+new Date().toString().substring(16,24)+"] Response recieved for "+path);
+			console.log("["+getDate()+"] Response recieved for "+path);
 			handler(xhttp.responseText);
 		}
 	};
-	console.log("["+new Date().toString().substring(16,24)+"] Sending request for "+path);
+	console.log("["+getDate()+"] Sending request for "+path);
 	xhttp.open("GET",path,true);
 	xhttp.send();
 }
 function loadTextFile(path,textHandler) {
-	getFile(path,function(data) {textHandler(data.split('\n'));});
+	getFile(path,function(data) {textHandler(data.split('\n'))});
 }
 function insertCodeFromFile(path,elementId) {
 	loadTextFile(path,function(code) {
