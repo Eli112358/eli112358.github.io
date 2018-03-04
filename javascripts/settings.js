d848a857bf68f4e1a504eca5592243e7a6c08fe1
Eli112358 <Eli112358@users.noreply.github.com>
2018-03-03 22:33:20 -0500

    Fix settings save and load Fix settings snippets

--- javascripts/settings.js
+++ javascripts/settings.js
@@ -16,28 +16,38 @@ function initSettings(spec) {
               spec.set(initModule('settings-', ['main', 'body', 'open', 'close']));
               spec.get().color = initModule('settings-color-', colors);
               ['store', 'toggle'].forEach((n) => {
-                spec.get()[n] = initModule(`settings-${n}-`, spec[n]);
+                if(spec.hasOwnProperty(n))
+                  spec.get()[n] = initModule(`settings-${n}-`, spec[n]);
               });
               spec.get().save = (ele) => {localStorage[ele.id] = ele.value};
               spec.get().load = (ele) => {
-                var storedValue = localStorage[ele.id];
-                if(storedValue) ele.value = storedValue;
+                if(localStorage.hasOwnProperty(ele.id))
+                  ele.value = localStorage[ele.id];
               };
-              spec.get().forEach = (func) => {
-                ['color', 'store'].forEach((n) => {
-                  spec.get()[n].ele.forEach(func)
+              if(spec.hasOwnProperty('store')) {
+                spec.store.forEach((n) => {
+                  spec.get().load(spec.get().store.ele[n])
                 })
-              };
-              spec.get().forEach(spec.get().load);
-              spec.get().model = initModel(spec.get(), () => {spec.get().forEach(spec.get().save)});
+              }
               spec.get().color.set = (n, val) => {
                 document.documentElement.style.setProperty(`--${n}`, val)
               };
               colors.forEach((n) => {
+                spec.get().load(spec.get().color.ele[n]);
                 spec.get().color.set(n, spec.get().color.ele[n].value);
                 spec.get().color.ele[n].addEventListener('change', function() {
                   spec.get().color.set(n, this.value)
                 });
+              });
+              spec.get().model = initModel(spec.get(), () => {
+                colors.forEach((n) => {
+                  spec.get().save(spec.get().color.ele[n])
+                });
+                if(spec.hasOwnProperty('store')) {
+                  spec.store.forEach((n) => {
+                    spec.get().save(spec.get().store.ele[n])
+                  })
+                }
               });
               spec.get().toggle.setup = (spec1) => {
                 spec1.ele.onclick = () => {
