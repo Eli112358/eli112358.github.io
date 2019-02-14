39effd3596f08b460f9cd100ff45e3f133f363bf
Eli112358 <Eli112358@users.noreply.github.com>
2019-02-13 21:17:34 -0500

    Spaces to tabs

--- javascripts/modifiable-list.js
+++ javascripts/modifiable-list.js
@@ -1,28 +1,28 @@
 function initModifiableList(spec) {
-  spec.set(initModule(spec.prefix, ['main', 'list', 'index', 'value', 'add']));
-  spec.get().array = [];
-  spec.get().ele['add'].onclick = () => {
-    spec.get().set({
-      'index': spec.get().ele.index.value,
-      'value': spec.get().ele.value.value
-    })
-  };
-  spec.get().set = (spec1) => {
-    spec.get().array[spec1.index] = spec1.value;
-    spec.get().redraw();
-  };
-  spec.get().save = () => {
-    localStorage[spec.get().ele.list.id] = spec.get().array;
-  };
-  spec.get().load = () => {
-    spec.get().array = localStorage[spec.get().ele.list.id];
-    spec.get().redraw();
-  };
-  spec.get().redraw = () => {
-    spec.get().ele.list.innerHTML = '';
-    for(var i in spec.get().array) {
-      if(!spec.get().array.hasOwnProperty(i)) continue;
-      spec.get().ele.list.innerHTML += `<li value="${i}">${spec.get().array[i]}\t<span class="close" onclick="${spec.get()}.set({'index': ${i}})">&times;</span></li>`;
-    }
-  };
+	spec.set(initModule(spec.prefix, ['main', 'list', 'index', 'value', 'add']));
+	spec.get().array = [];
+	spec.get().ele['add'].onclick = () => {
+		spec.get().set({
+		'index': spec.get().ele.index.value,
+		'value': spec.get().ele.value.value
+		})
+	};
+	spec.get().set = (spec1) => {
+		spec.get().array[spec1.index] = spec1.value;
+		spec.get().redraw();
+	};
+	spec.get().save = () => {
+		localStorage[spec.get().ele.list.id] = spec.get().array;
+	};
+	spec.get().load = () => {
+		spec.get().array = localStorage[spec.get().ele.list.id];
+		spec.get().redraw();
+	};
+	spec.get().redraw = () => {
+		spec.get().ele.list.innerHTML = '';
+		for(var i in spec.get().array) {
+		  if(!spec.get().array.hasOwnProperty(i)) continue;
+		  spec.get().ele.list.innerHTML += `<li value="${i}">${spec.get().array[i]}\t<span class="close" onclick="${spec.get()}.set({'index': ${i}})">&times;</span></li>`;
+		}
+	};
 }
--- javascripts/module.js
+++ javascripts/module.js
@@ -8,16 +8,16 @@ var hide = (ele) => {setHidden(ele, 'add')};
 var show = (ele) => {setHidden(ele, 'remove')};
 var toggle = (ele) => {setHidden(ele, 'toggle')};
 function initModule(prefix, elementNames) {
-  var module = {};
-  module.ele = getElements(prefix, elementNames);
-  return module;
+	var module = {};
+	module.ele = getElements(prefix, elementNames);
+	return module;
 }
 function getElements(prefix, names) {
-  var ele = {};
-  names.forEach((n) => {ele[n] = getById(prefix + n)});
-  return ele;
+	var ele = {};
+	names.forEach((n) => {ele[n] = getById(prefix + n)});
+	return ele;
 }
 function toggleButton(spec) {
-  if(spec.stored) localStorage[spec.ele.id] = !parseBool(localStorage[spec.ele.id]);
-  spec.ele[spec.property] = spec.values[1-spec.values.indexOf(spec.ele[spec.property])];
+	if(spec.stored) localStorage[spec.ele.id] = !parseBool(localStorage[spec.ele.id]);
+	spec.ele[spec.property] = spec.values[1-spec.values.indexOf(spec.ele[spec.property])];
 }
