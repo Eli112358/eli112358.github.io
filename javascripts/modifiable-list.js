4965ed359fa2c9614e38c29641e2471055f4ee20
Eli112358 <Eli112358D@gmail.com>
2020-04-09 19:20:37 -0400

    Deprecate initModifiableList, superseded by class

--- javascripts/modifiable-list.js
+++ javascripts/modifiable-list.js
@@ -1,71 +1,102 @@
 function initModifiableList(spec) {
-	spec.id = 'modifiable-list';
-	spec.preAppend = (code) => {
-		for (var i = 0; i < code.length; i++) {
-			while (code[i].indexOf('{prefix}-') > -1) {
-				code[i] = code[i].replace('{prefix}-', spec.prefix)
-			}
-		}
-	};
-	spec.postAppend = () => {
-		var ids = ['main', 'form', 'list', 'index', 'value'];
-		spec.set(initModule(spec.prefix, ids));
+	console.warn("WARNING! 'initModifiableList' is deprecated, please use 'new ModifiableList' instead!");
+	spec.set(new ModifiableList(spec));
+}
 
-		ids.forEach((id) => {
-			spec.get().ele[id].classList.add(`modifiable-list-${id}`);
+class ModifiableList {
+	array = [];
+	names = [
+		'form',
+		'index',
+		'list',
+		'main',
+		'value'
+	];
+	emptyCells = new Array(3);
+	newCell() {
+		return document.createElement('td');
+	}
+	constructor(args) {
+		this.args = args;
+		this.args.id = 'modifiable-list';
+		[
+			'callbackAfter',
+			'callbackBefore',
+			'postAppend',
+			'preAppend'
+		].forEach((name) => {
+			this.args[name] = this[name].bind(this);
 		});
-
-		spec.get().array = [];
-		spec.get().ele.form.action = 'javascript:void(0);';
-		spec.get().ele.form.onsubmit = () => {
-			spec.get().set({
-				'index': spec.get().ele.index.value,
-				'value': spec.get().ele.value.value
+		loadSnippets(this.args);
+	}
+	preAppend(code) {
+		console.warn("WARNING! [ModifiableList] 'preAppend' is deprecated, please use 'callbackBefore' instead!");
+		return this.callbackBefore(code);
+	}
+	postAppend() {
+		console.warn("WARNING! [ModifiableList] 'postAppend' is deprecated, please use 'callbackAfter' instead!");
+		this.callbackAfter();
+	}
+	appendRow(cells) {
+		let row = document.createElement('tr');
+		cells.forEach(row.appendChild.bind(row));
+		this.module.elements.list.appendChild(row);
+	}
+	load() {
+		this.array = localStorage[this.module.elements.list.id];
+		this.redraw();
+	}
+	save() {
+		localStorage[this.module.elements.list.id] = this.array;
+	}
+	set(i, value) {
+		if (value) {
+			this.array[i] = value;
+		} else {
+			delete this.array[i];
+		}
+		this.redraw();
+	}
+	callbackBefore(code) {
+		let replace = (s) => s.replace(/{prefix}-/g, this.args.prefix);
+		if (typeof code == 'object') {
+			code.forEach((item, i) => {
+				code[i] = replace(code[i]);
 			});
-			spec.get().ele.index.value = '';
-			spec.get().ele.value.value = '';
-		};
-		spec.get().set = (spec1) => {
-			spec.get().array[spec1.index] = spec1.value;
-			spec.get().redraw();
-		};
-		spec.get().save = () => {
-			localStorage[spec.get().ele.list.id] = spec.get().array;
-		};
-		spec.get().load = () => {
-			spec.get().array = localStorage[spec.get().ele.list.id];
-			spec.get().redraw();
-		};
-		spec.get().redraw = () => {
-			spec.get().ele.list.innerHTML = '';
-			for(var i in spec.get().array) {
-				if(!spec.get().array.hasOwnProperty(i)) continue;
-				if(!spec.get().array[i]) continue;
-
-				var data = [];
-				for (var j = 0; j < 3; j++) {
-					data.push(document.createElement('td'));
-				}
-
-				data[0].classList.add('close');
-				data[0].innerHTML = '&times;';
-				data[0].dataset.index = i;
-				data[0].onclick = () => {
-					var target = window.event.target;
-					spec.get().set({'index': parseInt(target.dataset.index)});
-				};
-
-				data[1].innerText = `${i}:`;
-
-				data[2].classList.add('modifiable-list-value');
-				data[2].innerText = spec.get().array[i];
-
-				var row = document.createElement('tr');
-				data.forEach((cell) => {row.appendChild(cell)});
-
-				spec.get().ele.list.appendChild(row);
-			}
-		};
-	};
-	loadSnippets(spec);
+		} else {
+			return replace(code);
+		}
+	}
+	callbackAfter() {
+		this.module = new Module(this.args.prefix, this.names);
+		this.names.forEach((name) => {
+			this.module.elements[name].classList.add(`${this.args.id}-${name}`);
+		});
+		this.module.elements.form.action = 'javascript:void(0);';
+		this.module.elements.form.onsubmit = this.submit.bind(this);
+		this.formFields = ['index', 'value'].map((name) => this.module.elements[name]);
+		this.formFieldValues = this.formFields.map.bind(this.formFields, (field) => field.value);
+	}
+	redraw() {
+		this.module.elements.list.innerHTML = '';
+		this.array.forEach((item, i) => {
+			let [remove, number, word] = Array.from(this.emptyCells, this.newCell);
+			remove.classList.add('close');
+			remove.innerHTML = '&times;';
+			remove.dataset.index = i;
+			remove.onclick = () => {
+				this.set(parseInt(window.event.target.dataset.index));
+			};
+			number.innerText = `${i}:`
+			word.classList.add(`${this.args.id}-value`);
+			word.innerText = item;
+			this.appendRow([remove, number, word]);
+		});
+	}
+	submit() {
+		this.set(...this.formFieldValues());
+		this.formFields.forEach((field) => {
+			field.value = '';
+		});
+	}
 }
