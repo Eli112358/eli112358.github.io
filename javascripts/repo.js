f717bbcfb4c085fd7236f24f6fa4d8745e7085bf
Eli112358 <Eli112358D@gmail.com>
2020-04-15 22:51:28 -0400

    Update repo and usage pages

--- javascripts/repo.js
+++ javascripts/repo.js
@@ -1,74 +1,83 @@
-function getPath(repo,file) {
-	return `repositories/${repo}/${file}`;
-}
-function getById(id) {
-	return document.getElementById(id);
-}
-function loadRepo() {
-	insertCodeFromFile({'path': 'snippets/header.txt', 'element': document.getElementById('downloads'), 'func': () => {}});
-	var repo=getURLParameter("repo");
-	var file=getURLParameter("file");
-	var isFile=file!==null;//typeof file!=="undefined"||
-	var repoName=getById("repo-name");
-	var release=getById("release");
-	var repoUrl="https://github.com/eli112358/"+repo;
-	repoName.innerHTML=repo;
-	release.href=repoUrl+"/releases/latest";
-	document.title=document.title+" - "+(isFile?file:repo);
-	if(isFile) {
-		repoName.href="repo.html?repo="+repo;
-		var usage=getById("usage");
-		var fileName=getById("file-name");
-		var prefOptions=["indented","inline","tooltip"];
-		var cname="descriptionLocation";
-		if(getCookie(cname)=="")setCookie(cname,prefOptions[0],365);
-		var pref=getCookie(cname);
-		fileName.href=repoUrl+"/blob/master/"+file;
-		fileName.innerHTML=file;
-		var prefId=prefOptions.indexOf(pref);
-		var descriptionOnLeft=true;
-		var columnTitles=new Array();
-		columnTitles[0]="Item";
-		columnTitles[1]="Description";
-		if(descriptionOnLeft)columnTitles.reverse();
-		var columns=document.createElement("table");
-		if(prefId==2) {
-			columns.id="columns";
-			columns.innerHTML+=`<tr><th>${columnTitles[0]}</th><th>${columnTitles[1]}</th></tr>`
+async function loadRepo() {
+	await new RemoteFile(new Header()).insertCodeFromFile();
+	let repo = urlParameters.repo;
+	let file = urlParameters.file;
+	let repoName = document.querySelector('#repo-name');
+	let release = document.querySelector('#release');
+	let repoUrl = `https://github.com/Eli112358/${repo}`;
+	repoName.innerHTML = repo;
+	release.href = `${repoUrl}/releases/latest`;
+	document.title = `${document.title} - ${file ? file : repo}`;
+	if (file) {
+		repoName.href = `repo.html?repo=${repo}`;
+		let usage = document.querySelector('#usage');
+		let fileName = document.querySelector('#file-name');
+		let options = [
+			'indented',
+			'inline',
+			'tooltip',
+		];
+		let storagePath = 'description.location';
+		if (!truStorage.getItem(storagePath)) {
+			if (!truStorage.getItem('description')) {
+				truStorage.setItem('description', {});
+			}
+			truStorage.setItem(storagePath, options[0]);
+		}
+		let preference = truStorage.getItem(storagePath);
+		fileName.href = `${repoUrl}/blob/master/${file}`;
+		fileName.innerHTML = file;
+		let preferenceIndex = options.indexOf(preference);
+		let descriptionOnLeft = true;
+		let columnTitles = new Array();
+		columnTitles[0] = 'Item';
+		columnTitles[1] = 'Description';
+		if (descriptionOnLeft) {
+			columnTitles.reverse();
+		}
+		let columns = document.createElement('table');
+		if (preferenceIndex == 2) {
+			columns.id = 'columns';
+			columns.insertAdjacentHTML('beforeEnd', `<tr><th>${columnTitles[0]}</th><th>${columnTitles[1]}</th></tr>`);
 			usage.appendChild(columns);
 		}
-		getFile(getPath(repo,file+".json"),function(data){
-			function loadItem(item){
-				switch(prefId) {
-					case 0:
-						usage.innerHTML+=`<p>${item.item}</p><p class="description indented">${item.description}</p>`;
-						//future idea: indent length set by cookie via attributes
-						break;
-					case 1:
-						var tdText=new Array();
-						tdText[0]=item.item;
-						tdText[1]=item.description;
-						if(descriptionOnLeft)tdText.reverse();
-						columns.innerHTML+=`<tr><td>${tdText[0]}</td><td>${tdText[1]}</td></tr>`;
-						break;
-					case 2:
-						usage.innerHTML+=`<div class="tooltip">${item.item}<span class="tooltiptext">${item.description}</span></div>`;
-						break;
-					default:
-						console.log(`Invalid value for ${cname}: `+pref);
-				}
+		let path = `repositories/${repo}/${file}.json`;
+		let handler = {
+			handle: (data) => {
+				Object.values(JSON.parse(data).items).forEach((item) => {
+					console.debug(item);
+					switch (preferenceIndex) {
+						case 0:
+							usage.insertAdjacentHTML('beforeEnd', `<p>${item.item}</p><p class="description indented">${item.description}</p>`);
+							break;
+						case 1:
+							let tdText = [item.item, item.description];
+							if (descriptionOnLeft) {
+								tdText.reverse();
+							}
+							columns.insertAdjacentHTML('beforeEnd', `<div class="tooltip">${item.item}<span class="tooltiptext">${item.description}</span></div>`);
+							break;
+						case 2:
+							usage.insertAdjacentHTML('beforeEnd', `<div class="tooltip">${item.item}<span class="tooltiptext">${item.description}</span></div>`);
+							break;
+						default:
+							console.log(`Invalid value for ${storagePath}: ${preferenceIndex}`);
+					}
+				});
 			}
-			var obj=JSON.parse(data);
-			for(var x in obj.items)loadItem(obj.items[x]);
-		});
+		};
+		await new RemoteFile({path, handler}).getFile();
 	} else {
-		repoName.href=repoUrl;
-		var filesList=getById("files");
-		getFile({"path": getPath(repo,"files.txt"), "handler": function(data) {
-			var lines=data.split('\n');
-			for(var x=0;x<lines.length;x++){
-				filesList.innerHTML+=`<li><a href="usage.html?repo=${repo}&file=${lines[x]}">${lines[x]}</a></li>`;
+		repoName.href = repoUrl;
+		let filesList = document.querySelector('#files');
+		let path = `repositories/${repo}/files.txt`;
+		let handler = {
+			handle: (data) => {
+				data.split('\n').forEach((line) => {
+					filesList.insertAdjacentHTML('beforeEnd', `<li><a href="usage.html?repo=${repo}&file=${line}">${line}</a></li>`);
+				});
 			}
-		}});
+		};
+		await new RemoteFile({path, handler}).getFile();
 	}
 }
