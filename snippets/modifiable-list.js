7d3306fb94b5e9a5cb530eacc5e4edb0f95a362f
Eli112358 <Eli112358D@gmail.com>
2020-07-27 16:02:10 -0400

    Adjust indentation in modifiable list snippet

--- snippets/modifiable-list.js
+++ snippets/modifiable-list.js
@@ -1,13 +1,13 @@
 function getCode(prefix) {
 	return [
 		`<div id="${prefix}-main">`,
-		`<form id="${prefix}-form">`,
-		`<input id="${prefix}-index" type="number">`,
-		`<input id="${prefix}-value">`,
-		'<input type="submit" value="Add">',
-		'</form>',
-		'<br/>',
-		`<table id="${prefix}-list"></table>`,
+			`<form id="${prefix}-form">`,
+				`<input id="${prefix}-index" type="number">`,
+				`<input id="${prefix}-value">`,
+				'<input type="submit" value="Add">',
+			'</form>',
+			'<br/>',
+			`<table id="${prefix}-list"></table>`,
 		'</div>'
 	].join('');
 }
